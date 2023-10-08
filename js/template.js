6fcc64ea0e0de5d4eab5668124d9b19023b9defe
ri0prz <142315404+ri0prz@users.noreply.github.com>
2023-10-08 20:42:34 +0700

    instruction section created

--- js/template.js
+++ js/template.js
@@ -2,9 +2,7 @@
 const mobileBar = document.querySelector('.mobile-effect');
 const slideBar = document.querySelector('nav .right');
 
-if (window.innerWidth < 767) {
-   mobileBar.onclick = () => {
-      mobileBar.classList.toggle('active');
-      slideBar.classList.toggle('active');
-   }
+mobileBar.onclick = () => {
+   mobileBar.classList.toggle('active');
+   slideBar.classList.toggle('active');
 }
