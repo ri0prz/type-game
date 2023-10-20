1a8652f810de2774e79e49c8963721ebea1f958d
ri0prz <142315404+ri0prz@users.noreply.github.com>
2023-10-20 12:56:49 +0700

    update

--- php/function.php
+++ php/function.php
@@ -1,54 +1,57 @@
 <?php
 
-// koneksi ke database
-$db = mysqli_connect("localhost","root","","TypeGame");
+// Koneksi ke database
+$db = mysqli_connect("localhost", "root", "", "TypeGame");
 
-// baca isi yang ada didatabase
-function query($query){
+// Baca isi yang ada didatabase
+function query($query)
+{
     global $db;
     $result = mysqli_query($db, $query);
     $rows = [];
-    while($row = mysqli_fetch_assoc($result)){
+    while ($row = mysqli_fetch_assoc($result)) {
         $rows[] = $row;
     }
-    return  $rows;
+    return $rows;
 }
 
-// registrasi//
-function registrasi($tambah){
+// Registrasi
+function registrasi($tambah)
+{
     global $db;
 
-    $username = strtolower(stripslashes($tambah['username']));  //stripslashes adalah untuk membersihkan dari karakter2 aneh
+    $username = strtolower(stripslashes($tambah['username'])); // ✨ stripslashes membersihkan dari karakter2 aneh
     $password = $tambah['password'];
     $password2 = $tambah['password2'];
 
-    // cek apakah username sudah pernah ada atau belum 
+    // Cek username ada belum 
     $result = mysqli_query($db, "SELECT username FROM user WHERE username = '$username'");
 
-    if(mysqli_fetch_assoc($result)){
+    if (mysqli_fetch_assoc($result)) {
         echo "<script>
         alert('username sudah pernah ada')
         </script>";
         return false;
     }
-    
-    // cek konfir password
-    if($password !== $password2){
+
+    // Cek konfirmasi password
+    if ($password !== $password2) {
         echo "<script>
         alert('password salah')
         </script>";
         return false;
     }
 
-    // enksripsi password
-    $password = password_hash($password, PASSWORD_DEFAULT); //pakai parameter password_default aja
+    // Enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
     // tambahkan username ke database
     $query = "INSERT INTO user VALUES (
     '',
     '$username',
     '$password'
     )";
-    mysqli_query($db,$query);
+    mysqli_query($db, $query);
 
     return mysqli_affected_rows($db);
 }
--- php/register.php
+++ php/register.php
@@ -2,13 +2,13 @@
 require "function.php";
 
 // cek apakah button di klik atau tidak 
-if(isset($_POST['submit'])){
-    if(registrasi($_POST) > 0 ){
+if (isset($_POST['submit'])) {
+    if (registrasi($_POST) > 0) {
         echo "<script>
         alert('berhasil')
         document.location.href = 'login.php';
         </script>";
-    }else{
+    } else {
         echo "<script>
         alert('gagal')
         document.location.href = 'register.php';
@@ -20,25 +20,87 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+
+    <!-- Plugins -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+
+    <!-- Styles -->
+    <style>
+        label {
+            align-self: flex-start;
+            color: gray;
+        }
+
+        input {
+            border: none;
+            border-bottom: 2px solid black;
+            outline: none;
+            padding: 8px 4px;
+            margin-bottom: 8px;
+        }
+
+        button[type="submit"] {
+            margin: 20px 0;
+        }
+    </style>
 </head>
-<body>
 
-
-    <div class="container">
-        <form action="" method="post" class="form">
-            <label for="username">Username : </label>
-            <input type="text" name="username" id="username" placeholder="username" autocomplete="none">
-            <label for="password">password : </label>
-            <input type="password" name="password" id="password" placeholder="password" autocomplete="none">
-            <label for="password2">konfir password : </label>
-            <input type="password" name="password2" id="password2" placeholder="password2" autocomplete="none">
-        <button type="submit" name="submit" class="button">Login!!!</button>
-        </form>
-        <p class="text">sudah punya akun?Login <a href="login.php">Disini</a>.</p>
+<body class="d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
+    <div class="toast-container position-fixed bottom-0 end-0 p-4">
+        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header bg-danger-subtle">                
+                <strong class="me-auto">Error occured.</strong>
+                <small>recently</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                Data defined is not found.
+            </div>
+        </div>
     </div>
+
+    <div class="container d-flex flex-column justify-content-center align-items-center">
+        <h1 class="fw-bold mb-4" style="font-size: 2rem;">t<span class="text-success">y</span>pe!</h1>
+        <form action="" method="post" class="d-flex flex-column justify-content-center align-items-center">
+            <label for="username">Username.</label>
+            <input type="text" name="username" id="username" autocomplete="off"><br>
+
+            <label for="password">Password.</label>
+            <input type="password" name="password" id="password" autocomplete="off"><br>
+
+            <label for="password">Re-type Password.</label>
+            <input type="password" name="password2" id="password2" autocomplete="off">
+
+            <button class="btn btn-success w-100 py-2" name="submit" type="submit">Register</button>
+        </form>
+        <p style="word-spacing: -1px; color: darkgray;">Had account? <a href="login.php"
+                class="text-success fw-bold text-uppercase">click me</a></p>
+    </div>
+
+    <!-- Bootstrap Script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    <script>
+        const toastTrigger = document.getElementById('liveToastBtn')
+        const toastLiveExample = document.getElementById('liveToast')
+        const alertToast = () => {
+            const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+            toastBootstrap.show();
+        }        
+    </script>
+    <!-- Bootstrap Script -->
+
+    <?php if (isset($error)): ?>
+        <script>alertToast();</script>
+    <?php endif; ?>
 </body>
-</html> 
+
+</html>
