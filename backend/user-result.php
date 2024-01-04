d7d8807c897265b1fe86f2d9250a46533d9d3842
ri0prz <142315404+ri0prz@users.noreply.github.com>
2024-01-04 14:18:14 +0700

    normal update: db result synchronization

--- backend/user-result.php
+++ backend/user-result.php
@@ -8,23 +8,43 @@ $db = connectDb();
 // Check login state
 isLogin();
 
-function insertResultToDb($user_avg, $user_score, $grade_id, $user_id, $user_server, $user_gender) {
+function insertResultToDb($user_avg, $user_score, $grade_id, $user_id, $user_server, $user_gender)
+{
 
    global $db;
 
-   // Set query
-   $query = <<< SQL
+   $query = <<<SQL
       INSERT INTO `valuation_user` 
          (`valuation_id`, `valuation_rate`, `valuation_score`, `grade_id`, `user_id`, `gender_id`, `server_id`) 
       VALUES 
-         (NULL, ?, ?, ?, ?, ?, ?);
+         (NULL, :userAvg, :userScore, :gradeId, :userId, :userGender, :userServer);
+
+      DELETE FROM `valuation_user` WHERE (valuation_rate = 0 OR valuation_score = 0)
+      AND user_id = :userId;
    SQL;
 
    // Prepare
-   $statement = $db->prepare($query);
-   $statement->execute([$user_avg, $user_score, $grade_id, $user_id, $user_gender, $user_server]);
+   $statement = $db->prepare($query);   
+   $statement->bindParam("userAvg", $user_avg);
+   $statement->bindParam("userScore", $user_score);
+   $statement->bindParam("gradeId", $grade_id);
+   $statement->bindParam("userId", $user_id);
+   $statement->bindParam("userGender", $user_gender);
+   $statement->bindParam("userServer", $user_server);
 
-   echo "<script> alert('COOL!!') </script>";
+   // Execute
+   $statement->execute();
+   $statement->closeCursor();
+
+   // Recreate view
+   $view_query = <<<SQL
+      DROP VIEW IF EXISTS user_detail, user_display;
+      CALL dbViews();
+   SQL;
+
+   // Execute view
+   $db->exec($view_query);
+
 }
 
 ?>
